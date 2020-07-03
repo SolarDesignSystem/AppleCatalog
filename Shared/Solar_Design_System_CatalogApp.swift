a69f742373c983ca499e141f9b5a68e74a1a3d13
Brandon McQuilkin <brandon.mcquilkin@gmail.com>
2020-07-03 10:36:24 -0400

    Initial Commit

---
+++ Shared/Solar_Design_System_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_Design_System_CatalogApp.swift
+//  Shared
+//
+//  Created by Brandon McQuilkin on 7/3/20.
+//
+
+import SwiftUI
+
+@main
+struct Solar_Design_System_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
